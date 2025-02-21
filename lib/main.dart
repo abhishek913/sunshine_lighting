6558c10efe2f47b49c4096a696b770c55766b9e3
abhishek913 <74429793+abhishek913@users.noreply.github.com>
2025-02-21 05:34:08 -0500

    start home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:sunshine_lighting/login_page.dart';
+import 'package:sunshine_lighting/login/login_page.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 void main() async {
